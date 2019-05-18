e252374b212e37a0e95ed3296cc500bb056c9e6c
sk <stefankarlin@hotmail.com>
2019-05-18 11:28:20 +0200

    Replace for loop with while loop

--- snake_simple.m
+++ snake_simple.m
@@ -3,22 +3,23 @@ close all; clear all; clc
 %% Constants
 %Image operations
 alphaUnsharp = 10;
-stepSize = 25;
+stepSize = 50; %amount of starting point of snake
 
 %snake model
 alpha = 1.0;
 beta = 0.2;
 gamma = -100;
+epsilon = 0.02;
 
 %EdgeDetection
 useSobel = true; %for CannyFilter set to false
 thresHoldVal = 150; %for sobel Filter
 
 %% Input
-input_img = imread('data/simpleObjects.jpg');
+%input_img = imread('data/simpleObjects.jpg');
 %input_img = imread('tire.tif');
 %input_img = imread('eight.tif');
-%input_img = imread('AT3_1m4_07.tif'); %use the sobel filter; threshold:100
+input_img = imread('AT3_1m4_07.tif'); %use the sobel filter; threshold:100
 %input_img = imread('hands1.jpg');
 %input_img = imread('toyobjects.png');
 %input_img = imread('pillsetc.png'); %sobel filter; threshold:150 --> good example
@@ -39,7 +40,7 @@ imshow(input_grey)
 input_med = imageOperators.medianFilter(input_grey);
 
 
-%% Init snake
+%% Initialse snake
 fig2 = figure(2);
 
 imshow(input_med)
@@ -56,7 +57,7 @@ hold on,plot(xCenter, yCenter,'g*')
 
 % This are the initial values for the snake, estimated from the user input
 hold on, plot(xVals_opt,yVals_opt,'g-')
-%% Initialize snake model
+%% Initialise snake model
 % !! xVals are the columns and yVals are the rows in the image!!!
 snake = snakeModel.create(alpha,beta,gamma,xVals_opt,yVals_opt, input_med,useSobel,thresHoldVal);
 image = snake.edgeImage;
@@ -71,20 +72,30 @@ mesh(1:columns,1:rows,snakeEnergImg)
 figure(6)
 plot3(xVals_opt, yVals_opt, snake.energyValsInit)
 
-
+%% Minimize snake energy
 initEnergy = snake.totalEnergyInit;
-iterationsteps = 500;
-snakeEnergies = zeros(1,iterationsteps);
+iterationsteps = 1;
+snakeEnergies = [];
 figure(fig2)
 color = 'b-';
 snakeCont = plot(snake.xVals,snake.yVals, color);
-for i=1:iterationsteps
-    
+prevEnergy = snake.totalEnergy;
+runLoop = true;
+
+while runLoop 
+   
     snake = snake.minimizeEnergy(0.2);
-    snakeEnergies(i) = snake.totalEnergy;
+    snakeEnergies(iterationsteps) = snake.totalEnergy;
+    iterationsteps = iterationsteps + 1;
+    actualEnergy = snake.totalEnergy;
+    if abs(prevEnergy-actualEnergy) > epsilon
+        runLoop = true;
+        prevEnergy = actualEnergy;  
+    else
+        runLoop = false;
+    end
     
-    
-    if mod(i,2) ~= 0
+    if mod(iterationsteps,2) ~= 0
         color = 'r-';
     else
         color = 'b-';
@@ -92,8 +103,14 @@ for i=1:iterationsteps
     figure(fig2)
     delete(snakeCont)
     snakeCont = plot(snake.xVals,snake.yVals, color);
- 
+  
 end
 endEnergy = snake.totalEnergy;
 figure(7)
-plot(1:iterationsteps,snakeEnergies)
+plot(1:length(snakeEnergies),snakeEnergies)
+
+fig8 = figure(8);
+imshow(input_grey)
+figure(fig8)
+hold on,plot(snake.xVals,snake.yVals,'g-')
+title(["Final snake after ",num2str(iterationsteps), "iterations"])
