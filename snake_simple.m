ef47a221751e6e23544f133bf70982c707023d06
sk <stefankarlin@hotmail.com>
2019-05-19 10:36:59 +0200

    Add quiver plot

--- snake_simple.m
+++ snake_simple.m
@@ -10,6 +10,7 @@ alpha = 1.0;
 beta = 0.2;
 gamma = -100;
 epsilon = 0.02;
+deltaT = 1.0;
 
 %EdgeDetection
 useSobel = true; %for CannyFilter set to false
@@ -19,9 +20,9 @@ thresHoldVal = 150; %for sobel Filter
 %input_img = imread('data/simpleObjects.jpg');
 %input_img = imread('tire.tif');
 %input_img = imread('eight.tif');
-input_img = imread('AT3_1m4_07.tif'); %use the sobel filter; threshold:100
+%input_img = imread('AT3_1m4_07.tif'); %use the sobel filter; threshold:100
 %input_img = imread('hands1.jpg');
-%input_img = imread('toyobjects.png');
+input_img = imread('toyobjects.png');
 %input_img = imread('pillsetc.png'); %sobel filter; threshold:150 --> good example
 subplot(3,3,1)
 imshow(input_img)
@@ -61,31 +62,33 @@ hold on, plot(xVals_opt,yVals_opt,'g-')
 % !! xVals are the columns and yVals are the rows in the image!!!
 snake = snakeModel.create(alpha,beta,gamma,xVals_opt,yVals_opt, input_med,useSobel,thresHoldVal);
 image = snake.edgeImage;
-[rows,columns] = size(input_med);
+
 figure(4), imshow(snake.edgeImage)
 
 snakeEnergImg = snake.energyImage;
 
-figure(5)
-mesh(1:columns,1:rows,snakeEnergImg)
-
-figure(6)
-plot3(xVals_opt, yVals_opt, snake.energyValsInit)
 
 %% Minimize snake energy
 initEnergy = snake.totalEnergyInit;
 iterationsteps = 1;
 snakeEnergies = [];
+snakeXYVals = {};
 figure(fig2)
 color = 'b-';
 snakeCont = plot(snake.xVals,snake.yVals, color);
 prevEnergy = snake.totalEnergy;
 runLoop = true;
-
+cellIndex = 1;
 while runLoop 
    
-    snake = snake.minimizeEnergy(0.2);
+    snake = snake.minimizeEnergy(deltaT);
     snakeEnergies(iterationsteps) = snake.totalEnergy;
+    if mod(iterationsteps,10) == 0
+        snakeXYVals{1, cellIndex} = snake.xVals;
+        snakeXYVals{2, cellIndex} = snake.yVals;
+        cellIndex = cellIndex + 1;
+    end
+    
     iterationsteps = iterationsteps + 1;
     actualEnergy = snake.totalEnergy;
     if abs(prevEnergy-actualEnergy) > epsilon
@@ -103,8 +106,10 @@ while runLoop
     figure(fig2)
     delete(snakeCont)
     snakeCont = plot(snake.xVals,snake.yVals, color);
-  
+    
+    
 end
+
 endEnergy = snake.totalEnergy;
 figure(7)
 plot(1:length(snakeEnergies),snakeEnergies)
@@ -114,3 +119,20 @@ imshow(input_grey)
 figure(fig8)
 hold on,plot(snake.xVals,snake.yVals,'g-')
 title(["Final snake after ",num2str(iterationsteps), "iterations"])
+
+figure(9)
+[rows,columns] = size(snakeXYVals);
+n = length(snakeXYVals{1,1});
+ for i=1:columns-1
+     X = snakeXYVals{1,i};
+     Y = snakeXYVals{2,i};
+     X_next = snakeXYVals{1,i+1};
+     Y_next = snakeXYVals{2,i+1};
+     diffX = X_next - X;
+     diffY = Y_next - Y;
+     plot(X,Y),hold on
+     quiver(X,Y,diffX,diffY,0,'color',[1 0 0]),hold on
+end
+
+
+
